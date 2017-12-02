d11bfc43996349524c72be285f6de0241d696928
LiangYT <sinothk@126.com>
2017-12-02 22:53:39 +0800

    2017年12月2日 22:53:36

---
+++ DialogManager/src/main/java/com/sinothk/dialog/cityPicker/CityPickerView.java
@@ -0,0 +1,9 @@
+package com.sinothk.dialog.cityPicker;
+
+/**
+ * Created by 梁玉涛 on 2017/12/02.
+ * 功能：
+ */
+
+public class CityPickerView {
+}
---
+++ DialogManager/src/main/java/com/sinothk/dialog/datePicker/DatePickerView.java
@@ -0,0 +1,9 @@
+package com.sinothk.dialog.datePicker;
+
+/**
+ * Created by 梁玉涛 on 2017/12/02.
+ * 功能：
+ */
+
+public class DatePickerView {
+}
