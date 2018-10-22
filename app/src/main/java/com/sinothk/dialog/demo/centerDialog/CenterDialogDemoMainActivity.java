a4293c1e3ad1a2e69526c7156ba14f013d8e144e
LiangYT <381518188@qq.com>
2018-10-22 10:30:45 +0800

    调整

--- app/src/main/java/com/sinothk/dialog/demo/centerDialog/CenterDialogDemoMainActivity.java
+++ app/src/main/java/com/sinothk/dialog/demo/centerDialog/CenterDialogDemoMainActivity.java
@@ -11,6 +11,7 @@ public class CenterDialogDemoMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 弹出对话框！
         setContentView(R.layout.activity_center_dialog_demo_main);
     }
 
