687a16ad75bf258ae563390c0e5335e044be2acd
LiangYT <381518188@qq.com>
2019-10-26 15:10:45 +0800

    升级环境！

--- app/src/main/java/com/sinothk/dialog/demo/bottomDialog/BottomDialogDemoMainActivity.java
+++ app/src/main/java/com/sinothk/dialog/demo/bottomDialog/BottomDialogDemoMainActivity.java
@@ -2,9 +2,10 @@ package com.sinothk.dialog.demo.bottomDialog;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jiangyy.easydialog.SingleChoiceDialog;
 import com.sinothk.dialog.bottomDialog.normalDialog.BottomSelectorDialog;
